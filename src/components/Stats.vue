00b5b40704245e248d791f1d244c3fad66a58c19
Adebola Fagbowore <bfagbowore@gmail.com>
2019-10-02 11:51:11 +0100

    manage state using vuex using getters, actions and mutations

---
+++ src/components/Stats.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="stats">
+        <h1>A different component</h1>
+        <p>There are currently {{ countLinks }} links</p>
+
+        <button @click="removeAllLinks">Remove all links</button>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+
+import { mapGetters, mapMutations ,mapActions } from 'vuex'
+
+export default {
+    name: 'Stats',
+    data(){
+        return {
+            msg: ''
+        }
+    },
+    methods: {
+        ...mapGetters([
+            'countLinks'
+        ]),
+        ...mapMutations([
+            'REMOVE_ALL'
+        ]),
+        ...mapActions([
+            'removeAll'
+        ]),
+        removeAllLinks(){
+            this.removeAll().then(() => {
+                this.msg = 'All links removed'
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+    button{
+        padding: 10px;
+        margin-top: 30px;
+        width: 100%;
+        background: none;
+        border: 1px solid lightgrey;
+        outline: 0;
+        cursor: pointer;
+    }
+</style>
--- src/store.js
+++ src/store.js
@@ -5,12 +5,41 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    title: 'My Custom Title',
+    links: [
+      'http://google.com',
+      'http://youtube.com',
+    ]
   },
+  getters: {
+    countLinks: state => {
+      return state.links.length
+    }
+  },
+  // calling the mutations directly is for synchronous events
   mutations: {
-
+    ADD_LINK: (state, link) => {
+      state.links.push(link)
+    },
+    REMOVE_LINK: (state, link) => {
+      state.links.splice(link, 1)
+    },
+    REMOVE_ALL: (state) => {
+      state.links = []
+    },
   },
+  // use actions for asynchronous events
   actions: {
-
+    removeLink: (context, link) => {
+      context.commit("REMOVE_LINK", link)
+    },
+    removeAll({commit}) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit('REMOVE_ALL')
+          resolve()
+        }, 1500)
+      })
+    }
   }
 })
